56e23cc366e2340520b10b9f6f7f5caf3bb1f7cd
MuresHub Server <server@mures.hub>
2024-12-03 03:49:58 +0000

    Update

--- src/Air/Form/Element/Text.php
+++ src/Air/Form/Element/Text.php
@@ -6,8 +6,37 @@ namespace Air\Form\Element;
 
 class Text extends TextAbstract
 {
+  public ?string $type = 'text';
+
   /**
    * @var string|null
    */
   public ?string $elementTemplate = 'form/element/text';
+
+  /**
+   * @return string|null
+   */
+  public function getType(): ?string
+  {
+    return $this->type;
+  }
+
+  /**
+   * @param string|null $type
+   */
+  public function setType(?string $type): void
+  {
+    $this->type = $type;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getValue(): mixed
+  {
+    if ($this->type === 'number') {
+      return intval(parent::getValue());
+    }
+    return parent::getValue();
+  }
 }
